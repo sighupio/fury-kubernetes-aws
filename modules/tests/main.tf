0492b05861940f6a1da0b51ad80513c7cb964453
Luca Novara <luca@sighup.io>
2019-02-12 18:28:53 +0100

    updated Terraform tests

--- modules/tests/main.tf
+++ modules/tests/main.tf
@@ -7,3 +7,37 @@ module "test-aws-vpc" {
     "${file("fixtures/terraform.pub")}",
   ]
 }
+
+module "test-aws-kubernetes" {
+  source            = "../aws-kubernetes"
+  name              = "omega"
+  env               = "staging"
+  kube-master-count = 3
+  kube-master-type  = "t3.small"
+
+  kube-workers = [
+    {
+      kind  = "infra"
+      count = 2
+      type  = "t3.small"
+    },
+    {
+      kind  = "production"
+      count = 2
+      type  = "t3.small"
+    },
+    {
+      kind  = "staging"
+      count = 1
+      type  = "t3.small"
+    },
+  ]
+
+  kube-private-subnets = "${module.test-aws-vpc.private_subnets}"
+  kube-public-subnets  = "${module.test-aws-vpc.public_subnets}"
+  kube-domain          = "${module.test-aws-vpc.domain_zone}"
+
+  ssh-public-keys = [
+    "${file("fixtures/terraform.pub")}",
+  ]
+}
