99247ff4d4c227ea2a8074c64de50bea1e57b49d
Luca Novara <luca@sighup.io>
2019-02-12 18:21:40 +0100

    improved aws-kubernetes kube-master

--- modules/aws-kubernetes/kube-master.tf
+++ modules/aws-kubernetes/kube-master.tf
@@ -1,30 +1,34 @@
 resource "aws_instance" "k8s-master" {
-  count         = "${var.kube_master_count}"
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "${var.kube_master_type}"
-  key_name      = "${aws_key_pair.kube-key.id}"
-  subnet_id     = "${element(aws_subnet.kube-subnet.*.id, count.index)}"
-
-  iam_instance_profile = "${aws_iam_instance_profile.kubernetes.name}"
+  count                  = "${var.kube-master-count}"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "${var.kube-master-type}"
+  user_data              = "${data.template_cloudinit_config.config.rendered}"
+  subnet_id              = "${element(data.aws_subnet.private.*.id, count.index)}"
+  iam_instance_profile   = "${aws_iam_instance_profile.main.name}"
   vpc_security_group_ids = ["${aws_security_group.kubernetes-master.id}"]
 
   root_block_device {
-    volume_type = "gp2"
-    volume_size = "${var.root_device_size}"
+    volume_type           = "gp2"
+    volume_size           = "80"
     delete_on_termination = "true"
   }
 
-  # ebs_block_device {
-  #   device_name = "${var.docker_device_name}"
-  #   volume_type = "gp2"
-  #   volume_size = "${var.docker_device_size}"
-  #   delete_on_termination = "true"
-  # }
-
   tags {
-    Name = "kube-master-${var.cluster_name}-${var.environment}-${count.index+1}"
-    Role = "master"
-    Gated = "true"
-    KubernetesCluster = "${var.cluster_name}"
+    Name              = "kube-master-${var.name}-${var.env}-${count.index+1}"
+    Role              = "master"
+    Gated             = "true"
+    KubernetesCluster = "${var.name}-${var.env}"
   }
+}
+
+resource "aws_route53_record" "k8s-master" {
+  count   = "${var.kube-master-count}"
+  zone_id = "${var.kube-domain}"
+  name    = "master-${count.index+1}"
+  type    = "A"
+  ttl     = "600"
+
+  records = [
+    "${element(aws_instance.k8s-master.*.private_ip, count.index)}",
+  ]
 }
