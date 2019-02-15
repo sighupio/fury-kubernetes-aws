12182ec5e0b272dfe3877e2f28353bc0dc6d39a1
berat <berat@sighup.io>
2019-02-15 18:40:37 +0100

    external load balancer

--- modules/aws-kubernetes/alb_nodes.tf
+++ modules/aws-kubernetes/alb_nodes.tf
@@ -1,16 +1,16 @@
 resource "aws_lb" "k8s-nodes" {
-  name                             = "${var.cluster_name}-${var.environment}-nodes-a"
+  name                             = "${var.name}-${var.env}-nodes-a"
   internal                         = false
   load_balancer_type               = "application"
-  subnets                          = ["${flatten(aws_subnet.public-subnet.*.id)}"]
+  subnets                          = ["${flatten(data.aws_subnet.public.*.id)}"]
   security_groups                  = ["${aws_security_group.k8s-nodes.id}"]
   enable_deletion_protection       = false
   enable_cross_zone_load_balancing = false
   idle_timeout                     = 400
 
   tags {
-    Name        = "${var.cluster_name}-${var.environment}-nodes-alb"
-    Environment = "production"
+    Name  = "${var.name}-${var.env}-nodes-alb"
+    env   = "production"
   }
 }
 
@@ -31,13 +31,14 @@ resource "aws_lb_listener" "k8s-nodes-http" {
 }
 
 resource "aws_lb_listener" "k8s-nodes-https" {
+  count       = "${length(var.kube-lb-external-domains)}"
   load_balancer_arn = "${aws_lb.k8s-nodes.arn}"
   port              = "443"
   protocol          = "HTTPS"
 
   // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html
   ssl_policy      = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn = "${data.aws_acm_certificate.transactionale-certificate.arn}"
+  certificate_arn = "${element(data.aws_acm_certificate.certificate.*.arn, count.index)}"
 
   default_action {
     type             = "forward"
@@ -45,8 +46,11 @@ resource "aws_lb_listener" "k8s-nodes-https" {
   }
 }
 
-data "aws_acm_certificate" "transactionale-certificate" {
-  domain      = "*.transactionale.com"
+
+//look for aws_lb_listener_certificate
+data "aws_acm_certificate" "certificate" {
+  count       = "${length(var.kube-lb-external-domains)}"
+  domain      = "${var.kube-lb-external-domains[count.index]}"
   statuses    = ["ISSUED"]
   most_recent = true
 }
@@ -55,10 +59,10 @@ resource "aws_lb_target_group" "k8s-nodes" {
   name        = "k8s-nodes"
   port        = 31080
   protocol    = "HTTP"
-  vpc_id      = "${aws_vpc.kube-vpc.id}"
+  vpc_id      = "${data.aws_subnet.public.0.vpc_id}"
   target_type = "instance"
 
-  health_check {
+  health_check {  
     path                = "/healthz"
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -69,17 +73,17 @@ resource "aws_lb_target_group" "k8s-nodes" {
 }
 
 resource "aws_autoscaling_attachment" "k8s-nodes" {
-  autoscaling_group_name = "${aws_autoscaling_group.k8s-nodes.id}"
+  count                  = "${length(var.kube-workers)}"
+  autoscaling_group_name = "${element(aws_autoscaling_group.main.*.name, count.index)}"
   alb_target_group_arn   = "${aws_lb_target_group.k8s-nodes.arn}"
 }
 
 resource "aws_security_group" "k8s-nodes" {
-  name        = "${var.cluster_name}-${var.environment}-web"
-  description = "k8s ELB Security Group"
-  vpc_id      = "${aws_vpc.kube-vpc.id}"
+  name        = "${var.name}-${var.env}-web"
+  vpc_id      = "${data.aws_subnet.public.0.vpc_id}"
 
   tags {
-    Name = "${var.cluster_name}-k8s-elb-${var.environment}-web"
+    Name = "${var.name}-k8s-elb-${var.env}-web"
   }
 
   ingress {
