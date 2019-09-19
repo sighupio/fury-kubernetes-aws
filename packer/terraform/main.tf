693936321966e057677b9de66eb1d086ca2c83af
phisco <pscorso93@gmail.com>
2019-09-19 15:13:17 +0200

    terraform files moved

---
+++ packer/terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "sighupio"
+
+    workspaces {
+      name = "fury-kubernetes-aws"
+    }
+  }
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Usage = "packer_build"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+output "packer" {
+  value = <<EOF
+{
+  "variables": {
+    "aws_access_key": "{{env `AWS_ACCESS_KEY_ID`}}",
+    "aws_secret_key": "{{env `AWS_SECRET_ACCESS_KEY`}}"
+  },
+  "builders": [{
+    "type": "amazon-ebs",
+    "access_key": "{{user `aws_access_key`}}",
+    "secret_key": "{{user `aws_secret_key`}}",
+    "region": "${var.region}",
+    "vpc_id": "${aws_vpc.main.id}",
+    "instance_type": "t2.micro",
+    "source_ami_filter": {
+      "filters": {
+        "virtualization-type": "hvm",
+        "name": "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*",
+        "root-device-type": "ebs"
+      },
+      "owners": ["099720109477"],
+      "most_recent": true
+    },
+    "ssh_username": "ubuntu",
+    "ami_name": "KFD-Ubuntu-{{timestamp}}"
+  }]
+}
+EOF
+
+}
