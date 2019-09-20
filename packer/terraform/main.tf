dcf5ef5936c340033f73ae1f0266787f36ef5fcf
phisco <pscorso93@gmail.com>
2019-09-20 15:06:55 +0200

    Revert "destroying everything"
    This reverts commit 32ae07843c3c322728d4fc72a4abb365fce9b244.

--- packer/terraform/main.tf
+++ packer/terraform/main.tf
@@ -9,15 +9,6 @@ terraform {
   }
 }
 
-variable "kinds" {
-  default = ["Worker", "Master", "Bastion"]
-}
-
-variable "user" {
-  default = "ubuntu"
-}
-
-
 variable "region" {
   default = "eu-west-1"
 }
@@ -27,64 +18,72 @@ provider "aws" {
   region  = var.region
 }
 
-#resource "aws_vpc" "main" {
-#cidr_block = "10.0.0.0/16"
-#tags = {
-#Usage = "packer_build"
-#}
-#}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Usage = "packer_build"
+  }
+}
 
-#resource "aws_internet_gateway" "gw" {
-#vpc_id = aws_vpc.main.id
-#tags = {
-#Usage = "packer_build"
-#}
-#}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Usage = "packer_build"
+  }
+}
 
-#resource "aws_route" "gw" {
-#route_table_id         = aws_vpc.main.main_route_table_id
-#destination_cidr_block = "0.0.0.0/0"
-#gateway_id             = aws_internet_gateway.gw.id
-#}
+resource "aws_route" "gw" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
 
 
-#resource "aws_subnet" "main" {
-#vpc_id                  = aws_vpc.main.id
-#cidr_block              = "10.0.1.0/24"
-#map_public_ip_on_launch = true
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
-#tags = {
-#Usage = "packer_build"
-#}
-#}
+  tags = {
+    Usage = "packer_build"
+  }
+}
 
-#data "template_file" "builders" {
-#count    = length(var.kinds)
-#template = file("builders.tpl")
-#vars = {
-#vpc_id = aws_vpc.main.id
-#user   = var.user
-#kind   = var.kinds[count.index]
-#}
-#}
+variable "kinds" {
+  default = ["Worker", "Master", "Bastion"]
+}
 
-#data "template_file" "provisioners" {
-#count    = length(var.kinds)
-#template = file("provisioners.tpl")
-#vars = {
-#kind = var.kinds[count.index]
-#}
-#}
+variable "user" {
+  default = "ubuntu"
+}
 
-#data "template_file" "amis" {
-#template = file("amis.tpl")
-#vars = {
-#provisioners = join(", ", data.template_file.provisioners.*.rendered)
-#builders     = join(", ", data.template_file.provisioners.*.rendered)
-#user         = var.user
-#}
-#}
+data "template_file" "builders" {
+  count    = length(var.kinds)
+  template = file("builders.tpl")
+  vars = {
+    vpc_id = aws_vpc.main.id
+    user   = var.user
+    kind   = var.kinds[count.index]
+  }
+}
 
-#output "amis" {
-#value = data.template_file.amis.rendered
-#}
+data "template_file" "provisioners" {
+  count    = length(var.kinds)
+  template = file("provisioners.tpl")
+  vars = {
+    kind = var.kinds[count.index]
+  }
+}
+
+data "template_file" "amis" {
+  template = file("amis.tpl")
+  vars = {
+    provisioners = join(", ", data.template_file.provisioners.*.rendered)
+    builders     = join(", ", data.template_file.provisioners.*.rendered)
+    user         = var.user
+  }
+}
+
+output "amis" {
+  value = data.template_file.amis.rendered
+}
