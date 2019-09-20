57a2a61249a87c70ea4396fb5f3cfe96369afcb6
phisco <pscorso93@gmail.com>
2019-09-20 14:42:16 +0200

    templated packer json file generation

--- packer/terraform/main.tf
+++ packer/terraform/main.tf
@@ -49,39 +49,95 @@ resource "aws_subnet" "main" {
   }
 }
 
-output "packer" {
-  value = <<EOF
-{
-  "variables": {
-    "aws_access_key": "{{env `AWS_ACCESS_KEY_ID`}}",
-    "aws_secret_key": "{{env `AWS_SECRET_ACCESS_KEY`}}"
-  },
-  "builders": [{
+variable "kinds" {
+  default = ["Worker", "Master", "Bastion"]
+}
+
+variable "user" {
+  default = "ubuntu"
+}
+
+data "template_file" "builders" {
+  count    = len(var.kinds)
+  template = <<EOF
+  {
+    "name": "${kind}",
     "type": "amazon-ebs",
     "access_key": "{{user `aws_access_key`}}",
     "secret_key": "{{user `aws_secret_key`}}",
-    "region": "${var.region}",
+    "region": "eu-west-1",
     "subnet_filter": {
       "filters": {
         "tag:Usage": "packer_build"
       },
       "most_free": true,
       "random": false
-    }
-    "vpc_id": "${aws_vpc.main.id}",
+    },
+    "vpc_id": "${vpc_id}",
     "instance_type": "t2.micro",
     "source_ami_filter": {
       "filters": {
         "virtualization-type": "hvm",
-        "name": "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*",
+        "name": "ubuntu/images/hvm-ssd/ubuntu-xenial-18.04-amd64-server-*",
         "root-device-type": "ebs"
       },
       "owners": ["099720109477"],
       "most_recent": true
     },
-    "ssh_username": "ubuntu",
-    "ami_name": "KFD-Ubuntu-{{timestamp}}"
-  }]
+    "ssh_username": "${user}",
+    "ami_name": "KFD-Ubuntu-${kind}-{{timestamp}}"
+  }
+  EOF
+  vars = {
+    vpc_id = aws_vpc.main.id
+    user   = var.user
+    kind   = var.kinds[count.index]
+  }
 }
-EOF
+
+data "template_file" "provisioners" {
+  count    = len(var.kinds)
+  template = <<EOF
+  {
+    "type": "ansible",
+    "playbook_file": "playbook-${kind}.yaml",
+    "only": ["${kind}"]
+  }
+  EOF
+  vars = {
+    kind = var.kinds[count.index]
+  }
+}
+
+data "template_file" "amis" {
+  template = <<EOT
+  {
+    "variables": {
+      "aws_access_key": "{{env `AWS_ACCESS_KEY_ID`}}",
+      "aws_secret_key": "{{env `AWS_SECRET_ACCESS_KEY`}}"
+    },
+    "builders": [
+      ${builders}
+    ],
+    "provisioners": [
+      ${provisioners},
+      {
+        "type": "shell",
+        "inline": [
+          "rm /home/${user}/.ssh/authorized_keys"
+        ]
+      }
+
+    ]
+  }
+
+  EOT
+  vars = {
+    provisioners = join(", ", template_file.provisioners.*.rendered)
+    builders     = join(", ", template_file.provisioners.*.rendered)
+  }
+}
+
+output "amis" {
+  value = template_file.amis.rendered
 }
