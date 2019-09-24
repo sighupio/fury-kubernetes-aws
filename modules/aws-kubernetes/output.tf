051c283e0f28c4ae7d6e1ec31937da8a6c760260
phisco <pscorso93@gmail.com>
2019-09-24 13:57:44 +0200

    [ modules/aws-kubernetes ] optional ssh-private-key in hosts.ini

--- modules/aws-kubernetes/output.tf
+++ modules/aws-kubernetes/output.tf
@@ -24,13 +24,13 @@ ansible_python_interpreter=python3
 dns_server=${cidrhost(data.aws_vpc.main.cidr_block, 2)}
 
 [gated:vars]
-ansible_ssh_common_args='-o ProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p -q -i ${var.ssh-private-key} ubuntu@${var.kube-bastions[0]}"'
+ansible_ssh_common_args='-o ProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p -q ${var.ssh-private-key != "" ? "-i" : ""} ${var.ssh-private-key} ubuntu@${var.kube-bastions[0]}"'
 public_lb_address=${aws_lb.external.dns_name}
 
 [master:vars]
 etcd_initial_cluster='${join(",", formatlist("%s=https://%s:2380", data.template_file.k8s-master.*.rendered, aws_route53_record.k8s-master.*.fqdn))}'
 control_plane_endpoint=${aws_route53_record.control-plane.fqdn}
-dns_zone=${join(".",compact(split(".",data.aws_route53_zone.main.name)))}
+dns_zone=${join(".", compact(split(".", data.aws_route53_zone.main.name)))}
 
 EOF
 }
