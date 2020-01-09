44971df8feb932159a8c8de82d3346d5074f1ee5
Luca Zecca <l.zecca78@gmail.com>
2020-01-09 09:19:21 +0100

    expoential backoff Due to race condition on timing between provisioning and make the cluster up and running,i am introducing a first version of an exponential backoff. Probably the err_retry could be externalized in a source file like the alert one.

--- roles/kube-node-common/files/auto-join-node.sh
+++ roles/kube-node-common/files/auto-join-node.sh
@@ -2,7 +2,27 @@
 
 source /cloud-init-report-alert.sh
 
-
+function err_retry() {
+  local exit_code=$1
+  local attempts=$2
+  local sleep_millis=$3
+  shift 3
+  for attempt in `seq 1 $attempts`; do
+    if [[ $attempt -gt 1 ]]; then
+      echo "Attempt $attempt of $attempts"
+    fi
+    # This weird construction lets us capture return codes under -o errexit
+    "$@" && local rc=$? || local rc=$?
+    if [[ ! $rc -eq $exit_code ]]; then
+      return $rc
+    fi
+    if [[ $attempt -eq $attempts ]]; then
+      return $rc
+    fi
+    local sleep_ms="$(($attempt * $attempt * $sleep_millis))"
+    sleep "${sleep_ms:0:-3}.${sleep_ms: -3}"
+  done
+}
 # Join function
 # Downloads the kubeadm token and execute it
 # $1: bucket+key where token is located
@@ -24,11 +44,11 @@ n=0
 fail_join=1
 me=`basename "$0"`
 
-until [ $n -ge $RETRIES ]
-do
-   join $JOIN_TOKEN_URL && fail_join=0 && break
-   n=$[$n+1]
-   notify $ALERT_MANAGER_HOSTNAME "warning" "Cloud-init script is failing. /$me causes this warning"
-   sleep $RETRY_SECONDS
-done
-exit $fail_join
+MAX_ATTEMPTS=50
+
+SLEEP_MILLISECS=1000
+
+join_command=$(join $JOIN_TOKEN_URL && fail_join=0 && break)
+
+err_retry 2 $MAX_ATTEMPTS $SLEEP_MILLISECS $join_command || notify $ALERT_MANAGER_HOSTNAME "warning" "Cloud-init script is failing. /$me causes this warning"
+echo "fail all attempts to join cluster"; exit $fail_join
