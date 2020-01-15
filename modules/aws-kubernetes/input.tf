a8474ad0a66781b534393b908efa4c24618ddee8
Luca Zecca <l.zecca78@gmail.com>
2020-01-15 10:53:06 +0100

    moved module into modules/

--- modules/aws-kubernetes/input.tf
+++ modules/aws-kubernetes/input.tf
@@ -213,23 +213,3 @@ data "aws_route53_zone" "additional" {
   zone_id = "${var.additional-domain}"
 }
 
-variable "acms_config_list" {
-  type = "list"
-  description = "list of maps that describe acm_san and acm_domain_name for each acm we need to create"
-
-# acms_config_list = [
-#   {
-#     acm_domain_name                    = "pippo.it"
-#     sans_with_same_domain              = "*.pippo.it,paperino.pippo.it"
-#     validation_ttl                     = 60
-#     allow_validation_record_overwrite  = true
-#   },
-#   {
-#     acm_domain_name                    = "pluto.it"
-#     sans_with_same_domain              = "*.pluto.it,paperino.pluto.it"
-#     validation_ttl                     = 60
-#     allow_validation_record_overwrite  = true
-#   },
-# ]
-
-}
