7aef92ebcf1a2675068f6c671da972ca2eefd20a
Samuele Chiocca <samuele@sighup.io>
2020-01-15 10:14:25 +0100

    RDS serverless

---
+++ modules/aws-rds-serverless/input.tf
@@ -0,0 +1,115 @@
+variable "name" {
+  type        = "string"
+  description = "RDS cluster name"
+}
+
+variable "env" {
+  type        = "string"
+  description = "RDS cluster environment"
+}
+
+variable "region" {
+  type        = "string"
+  default     = "eu-west-1"
+  description = "AWS region"
+}
+
+variable "rds-user" {
+  type        = "string"
+  description = "RDS cluster username"
+}
+
+variable "rds-password" {
+  type        = "string"
+  description = "RDS cluster password"
+}
+
+variable "rds-engine" {
+  type        = "string"
+  description = "RDS cluster engine"
+  default     = "aurora"
+}
+
+variable "rds-engine-mode" {
+  type        = "string"
+  description = "RDS cluster engine version"
+  default     = "serverless"
+}
+
+variable "rds-engine-version" {
+  type        = "string"
+  description = "RDS cluster engine version"
+  default     = "5.6.10a"
+}
+
+variable "rds-backup-retention" {
+  type        = "string"
+  default     = 7
+  description = "RDS cluster backups retention days"
+}
+
+variable "rds-parameter-group-name" {
+  type        = "string"
+  default     = ""
+  description = "RDS cluster parameter group name"
+}
+
+variable "rds-parameter" {
+  type        = "list"
+  default     = []
+  description = "RDS parameters list"
+}
+
+variable "rds-port" {
+  type        = "string"
+  description = "RDS cluster clients port"
+}
+
+variable "rds-aurora-autopause" {
+  type        = "string"
+  description = "RDS cluster scaling configuration auto pause"
+  default     = "true"
+}
+
+variable "rds-aurora-max-capacity" {
+  type        = "string"
+  description = "RDS cluster scaling configuration max capacity"
+  default     = "4"
+}
+
+variable "rds-aurora-min-capacity" {
+  type        = "string"
+  description = "RDS cluster scaling configuration min capacity"
+  default     = "2"
+}
+
+variable "rds-aurora-seconds-until-auto-pause" {
+  type        = "string"
+  description = "RDS cluster scaling configuration seconds until auto pause"
+  default     = "3600"
+}
+
+variable "subnets" {
+  type        = "list"
+  description = "List of AWS subnet IDs"
+}
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "aws_subnet" "main" {
+  count = "${length(var.subnets)}"
+  id    = "${element(var.subnets, count.index)}"
+}
+
+variable "friendly_name" {
+  type        = "string"
+  description = "Friendly Cname Name"
+}
+
+variable "domain_name" {
+  type        = "string"
+  description = "The domain on which create the friendly name"
+  default     = ""
+}
---
+++ modules/aws-rds-serverless/rds.tf
@@ -0,0 +1,60 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.name}-${var.env}"
+  subnet_ids = ["${var.subnets}"]
+}
+
+resource "aws_security_group" "main" {
+  name_prefix = "${var.name}-${var.env}-rds"
+  vpc_id      = "${data.aws_subnet.main.0.vpc_id}"
+
+  ingress {
+    from_port   = "${var.rds-port}"
+    to_port     = "${var.rds-port}"
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_rds_cluster" "main" {
+  cluster_identifier      = "${var.name}-${var.env}-aurora"
+  vpc_security_group_ids  = ["${aws_security_group.main.id}"]
+  db_subnet_group_name    = "${aws_db_subnet_group.main.name}"
+  engine                  = "${var.rds-engine}"                #"aurora"
+  engine_version          = "${var.rds-engine-version}"        #"5.6.10a"
+  engine_mode             = "${var.rds-engine-mode}"           #"serverless"
+  master_username         = "${var.rds-user}"
+  master_password         = "${var.rds-password}"
+  backup_retention_period = "${var.rds-backup-retention}"
+  skip_final_snapshot     = true
+  apply_immediately       = true
+  deletion_protection     = true
+
+  scaling_configuration {
+    auto_pause               = "${var.rds-aurora-autopause}"
+    max_capacity             = "${var.rds-aurora-max-capacity}"
+    min_capacity             = "${var.rds-aurora-min-capacity}"
+    seconds_until_auto_pause = "${var.rds-aurora-seconds-until-auto-pause}"
+  }
+
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "${var.domain_name}."
+  private_zone = true
+}
+
+resource "aws_route53_record" "main" {
+  count   = "${length(var.friendly_name) == 0 ? 0 : 1}"
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "${var.friendly_name}.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_rds_cluster.main.endpoint}"]
+}
