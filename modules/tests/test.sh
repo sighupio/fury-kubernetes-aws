3595230b5ee310aaa60875b450fa1e6c8e7792eb
Samuele Chiocca <samuele@nutellino.it>
2020-01-25 20:08:18 +0100

    Removed .terraform on test.sh Reactivated plan step

--- modules/tests/test.sh
+++ modules/tests/test.sh
@@ -1,5 +1,5 @@
+rm -rf .terraform/
 terraform init
 terraform fmt
 terraform validate
-#terraform plan
-# plan on test disabled, kubernetes modules depends on data source that are not available at the start
+terraform plan
