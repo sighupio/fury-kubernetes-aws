d91b79d700272a39c8f779e1aa12ab8f44e0516b
Simone Bruzzese <sbruzzese@sighup.io>
2023-06-13 15:37:36 +0200

    Add license to eks-addons

--- modules/eks-addons/vpc_cni.tf
+++ modules/eks-addons/vpc_cni.tf
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name      = var.cluster_name
   addon_name        = "vpc-cni"
